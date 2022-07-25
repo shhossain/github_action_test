a843b17c2345177f28079e0b97fbb2ac1faae114
shhossain <hossain0338@gmail.com>
2022-07-25 15:45:23 +0600

    check

--- githubAction.py
+++ githubAction.py
@@ -1,1 +1,10 @@
-a = 'v0.0.1'; b = int(a.split('''.''')[-1]); c = b + 1; d = f'''{'.'.join(a.split('.')[0:-1] + [str(c)])}'''; print(d)
+a = 'v0.0.1'
+b = int(a.split('.')[-1]) if a.split('.')[-1] else 0
+c = b + 1
+d = '.'.join(a.split('.')[0:-1] + [str(c)])
+d = d if d != '1' else '0.0.1'
+d = d if 'v' in d else 'v' + d
+print(d)
+
+# one line
+a = 'v0.0.1'; b = int(a.split('.')[-1]) if a.split('.')[-1] else 0; c = b + 1; d = '.'.join(a.split('.')[0:-1] + [str(c)]) if c != 1 else '0.0.1'; d = d if 'v' in d else 'v' + d; print(d)
